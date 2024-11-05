7d04d1fa2b1d61210cf7c7f10c56ebdafe83f529
Hoomam <hoaz1600@student.miun.se>
2024-11-05 16:43:02 +0100

    Cleaned ImageDTO

--- ComputerVision.Data/Models/DTO/ImageDTO.cs
+++ ComputerVision.Data/Models/DTO/ImageDTO.cs
@@ -29,22 +29,3 @@ public class ImageDTO
     public FilterType Filter {  get; set; }
 }
 
-/*public class ImageUpdateDTO
-{
-
-[Required]
-public int Id { get; set; }
-
-[Required]
-[MaxLength(30)]
-public string? ImageName { get; set; }
-
-[Required]
-[MaxLength(50)]
-public string? ImageMaterial { get; set; }
-
-[Required]
-public string? ImageUrl { get; set; }
-
-public IFormFile? ImageFile { get; set; }
-}*/
